f58f6293216cbd7c957670d6cf01f51956772ab8
Maija Runce <maijarunce11@gmail.com>
2025-06-26 10:58:13 +0300

    14th

--- src/13-is-blank.ts
+++ src/13-is-blank.ts
@@ -4,6 +4,17 @@ export {};
  * Create a function called isBlank, which checks if passed string is blank or not
  */
 
+function isBlank(name: string | null ){
+    if (name && name.length > 0) {
+        if(!name.trim()){ 
+            return true
+        }
+        return false
+    }
+
+    return true
+}
+
 console.log(isBlank(null)); // Expected output: true
 console.log(isBlank("")); // Expected output: true
 console.log(isBlank(" ")); // Expected output: true
