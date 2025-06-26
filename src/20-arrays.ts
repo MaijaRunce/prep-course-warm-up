50218c9ac6e67eed23e47588212ac4151f2f9a44
Maija Runce <maijarunce11@gmail.com>
2025-06-26 15:06:26 +0300

    21th

--- src/20-arrays.ts
+++ src/20-arrays.ts
@@ -7,5 +7,27 @@ export {};
  *  - https://www.w3schools.com/js/js_arrays.asp
  *  - https://javascript.info/array
  */
+const stringToArray = (fullName: string[]) => {
+    if (fullName.length < 1) {
+        return null
+    }
+
+    const formattedArray: string[] = []
+
+    fullName.map((n) => formattedArray.push(...n.split(" ")))
+
+    return formattedArray
+}
 
 console.log(stringToArray(["John Doe"])); // Expected output: ['John', 'Doe']
+console.log(stringToArray(["John Doe", "Maija Punce"])); // Expected output: ['John', 'Doe', 'Maija', 'Punce']
+console.log(stringToArray(["John Doe", "Maija Dunce"])); // Expected output: ['John', 'Doe', 'Maija', 'Dunce']
+
+
+//Destructuring example
+
+const boys = ["boy1", "boy2", "boy3"]
+const girls = ["girls1", "girls2", "girls3"]
+const school = [...boys, ...girls]
+
+console.log(school)
