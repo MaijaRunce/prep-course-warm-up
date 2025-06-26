8fbccdb2bd2c25f333bba91fa914f87404aa06a8
Maija Runce <maijarunce11@gmail.com>
2025-06-26 10:22:26 +0300

    13th

--- src/12-truncate-string.ts
+++ src/12-truncate-string.ts
@@ -1,3 +1,11 @@
 export {};
 
+function truncateString (str:string, num: number){
+    if (str.length <= num) {
+        return str
+      }
+      
+      return str.slice(0, num) 
+    }
+
 console.log(truncateString("CODELEX", 4)); // Expected output: CODE
