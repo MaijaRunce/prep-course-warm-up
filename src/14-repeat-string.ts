b743066450ba45e9a91661330a3d2682168ef6ea
Maija Runce <maijarunce11@gmail.com>
2025-06-26 11:05:06 +0300

    15

--- src/14-repeat-string.ts
+++ src/14-repeat-string.ts
@@ -1,6 +1,9 @@
 export {};
 
-const repeatString = () => {};
+// const repeatString =
+    function repeatString (str: string, num: number) {
+        return str.repeat (num)
+    };
 
 console.log(repeatString("a", 4)); // Expected output: 'aaaa'
 console.log(repeatString("b", 5)); // Expected output: 'bbbbb'
