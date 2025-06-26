7060161d361dafbc1f0dc66ab4640bf1bfe25e92
Maija Runce <maijarunce11@gmail.com>
2025-06-26 13:50:14 +0300

    20th

--- src/19-currency-conversion.ts
+++ src/19-currency-conversion.ts
@@ -15,9 +15,13 @@ export {};
  */
 
 // You are allowed to change this function
-function convertToUSD(price) {}
+function convertToUSD(price: number) {
+    return ((price * 0.85)+ 0.01).toFixed(2)
+}
 // You are allowed to change this function
-function convertToBRL(price) {}
+function convertToBRL(price: number) {
+    return ((price * 0.15)+ 0.01).toFixed(2)
+}
 
 const product = "You don't know JS";
 const price = 12.5;
