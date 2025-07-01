767e1dce842fa57ec7421654d2a8d7507fba5f9f
Maija Runce <maijarunce11@gmail.com>
2025-07-01 21:02:46 +0300

    25

--- src/24-looping-with-conditions.ts
+++ src/24-looping-with-conditions.ts
@@ -1,5 +1,15 @@
-export {};
+function onlyTheAces(arr: string[]) {
+  const newArr: string[] = [];
 
-function onlyTheAces(arr) {}
+  for (const element of arr) {
+    if (element == "Ace") {
+      newArr.push(element);
+    }
+  }
+
+  console.log(newArr);
+}
+
+
 
 console.log(onlyTheAces(["Ace", "King", "Queen", "Jack", "Ace"])); // Expected result: ['Ace', 'Ace']
