058cd3f954635b6ddd752003317fa3bb601dcd9c
Maija Runce <maijarunce11@gmail.com>
2025-07-04 06:49:33 +0300

    30

--- src/29-draw-triangle.ts
+++ src/29-draw-triangle.ts
@@ -5,7 +5,11 @@ export {};
  *  - https://blog.codeanalogies.com/2017/11/07/javascript-for-loops-explained/
  */
 
-function draw() {}
+function draw(value: number) {
+    for (let i = 0; i <= value; i++ ) {
+        console.log("*".repeat(i))
+    }
+}
 
 draw(3);
 /* Expected output:
