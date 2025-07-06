40811ec8b0838a74216c0f48096c17aa2922053e
Maija Runce <maijarunce11@gmail.com>
2025-07-06 21:22:08 +0300

    32

--- src/31-writers.ts
+++ src/31-writers.ts
@@ -5,33 +5,51 @@ export {};
  * "Hi, my name is {firstName} {lastName}. I am {age} years old, and work as a {occupation}."
  */
 
-const writers = [
+type Writer = {
+  firstName: string;
+  lastName: string;
+  occupation: string;
+  age: number;
+  alive: boolean;
+};
+
+const writers: Writer[] = [
   {
     firstName: "Virginia",
     lastName: "Woolf",
     occupation: "writer",
     age: 59,
-    alive: false
+    alive: false,
   },
   {
     firstName: "Zadie",
     lastName: "Smith",
     occupation: "writer",
     age: 41,
-    alive: true
+    alive: true,
   },
   {
     firstName: "Jane",
     lastName: "Austen",
     occupation: "writer",
     age: 41,
-    alive: false
+    alive: false,
   },
   {
     firstName: "bell",
     lastName: "hooks",
     occupation: "writer",
     age: 64,
-    alive: true
-  }
+    alive: true,
+  },
 ];
+
+// filter masiva iebuveta funkcija, atfiltre visus dzivos, ar for of loopu izkonsole visus dzivos taja teikuma
+
+let alivewritersV = writers.filter((writer) => writer.alive == true);
+
+for (const alivewriter of alivewritersV){
+  console.log(`Hi, my name is ${alivewriter.firstName} ${alivewriter.lastName}. I am ${alivewriter.age} years old, and work as a ${alivewriter.occupation}.`)
+
+}
+
