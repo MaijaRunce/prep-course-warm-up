7acf379942b267b34d1acb0ad2be4aa38079b1f9
Maija Runce <maijarunce11@gmail.com>
2025-07-07 21:50:47 +0300

    34

--- src/34-protect-email.ts
+++ src/34-protect-email.ts
@@ -4,7 +4,20 @@ export {};
  * Create a function protectEmail which hides some symbols of the email
  */
 
+const protectEmail = (email: string) => {
+  for (let i = 0; i < protectEmail.length; i++) {
+    const parts = email.split("@");
+    console.log(parts);
+
+    if (parts[0].length < 3) {
+      return "..." + "@" + parts[1];
+    } else {
+        return parts[0].slice(0,3) +"..." + "@" + parts[1];
+    }
+  }
+};
+
 console.log(protectEmail("secret123@codelex.io")); // Expected result: sec...@codelex.io
 console.log(protectEmail("example@example.com")); // Expected result: exa...@example.com
-console.log(protectEmail('12345@example.com')); // Expected result: 12...@example.com
-console.log(protectEmail('12@example.com')); // Expected result: ...@example.com
+console.log(protectEmail("12345@example.com")); // Expected result: 12...@example.com
+console.log(protectEmail("12@example.com")); // Expected result: ...@example.com
